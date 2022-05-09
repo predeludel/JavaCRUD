870db175df78ac39e207424c6ce804fc9138fc30
predeludel <sinitsa.2003@yandex.ru>
2022-05-09 12:35:24 +0300

    CRUD City

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/example/demo/controller/CityController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.City;
+import com.example.demo.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CityController {
+    @Autowired
+    CityRepository cityRepository;
+
+    @GetMapping("/cities")
+    public Iterable<City> read() {
+        return cityRepository.findAll();
+    }
+
+    @PostMapping("/cities")
+    public City create(@RequestBody City city) {
+        return cityRepository.save(city);
+    }
+
+    @DeleteMapping("/cities/{id}")
+    public Boolean delete(@PathVariable Long id) {
+        if (cityRepository.existsById(id)) {
+            cityRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    @PutMapping("/cities")
+    public City update(@RequestBody City newCity){
+        if (cityRepository.findById(newCity.getId()).isPresent()){
+            City oldCity = cityRepository.findById(newCity.getId()).get();
+            oldCity.setName(newCity.getName());
+            return cityRepository.save(oldCity);
+        } else{
+            return cityRepository.save(newCity);
+        }
+    }
+
+}
